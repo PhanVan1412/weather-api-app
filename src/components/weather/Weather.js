f4af6cd5e2be9fd9e9d90680722f742bb4e5ad77
unknown <phanvantam2020@gmail.com>
2023-05-21 16:55:50 +0700

    update ui

--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import { Card } from 'semantic-ui-react';
+import { Button } from 'semantic-ui-react';
 import moment from 'moment/moment';
+import './Weather.css';
+
+const refresh = () => {
+    window.location.reload();
+}
 
 const Weather = ({weatherData}) => {
   return (
    
     <>
-        <div>Weather</div>
-        <Card>
-            <Card.Content>
-                <Card.Header className="header">Địa chỉ: {weatherData.name}</Card.Header>
-                <p>Nhiệt độ hiện tại: {weatherData.main.temp} &deg;C</p>
-                <p>Thời gian mặt trời mọc: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
-                <p>Thời gian mặt trời lặn: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
-                <p>Trạng thái thời tiết: {weatherData.weather[0].main}</p>
-                <p>Độ ẩm: {weatherData.main.humidity} %</p>
-                <p>{moment().format('dddd')}</p>
-                <p>Ngày: {moment().format('LL')}</p>
-            </Card.Content>
-        </Card>
+        <div className="main">
+            <div className="top">
+                <div className="box">
+                    <p className="header">{weatherData.name}</p>
+                    <p>{moment().format('LTS')}</p>
+                </div>
+                <Button className="button" inverted color='blue' circular icon='refresh' onClick={refresh} />
+            </div>
+            <div className="flex">
+                <p className="day">{moment().format('dddd')}, <span>{moment().format('LL')}</span></p>
+                <p className="description">{weatherData.weather[0].main}</p>
+            </div>
+
+            <div className="flex">
+                <p className="temp">Temprature: {weatherData.main.temp} &deg;C</p>
+                <p className="temp">Humidity: {weatherData.main.humidity} %</p>
+            </div>
+
+            <div className="flex">
+                <p className="sunrise-sunset">Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
+                <p className="sunrise-sunset">Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
+            </div>
+    </div>
     </>
   )
 }
